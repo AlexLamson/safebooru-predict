426210a2ef603f6509d64e2001236b048fe7ec54
AlexLamson <alex.lamson@gmail.com>
2016-11-20 11:08:48 -0500

    fixed sampling bug

--- src/2_sample/sample_data.py
+++ src/2_sample/sample_data.py
@@ -19,18 +19,22 @@ def get_num_lines(filename):
 
 
 def sample_every_nth_line(source, dest, n):
+	num_samples = 0
+	num_lines = get_num_lines(source)
 	with open(dest, "w+") as sample:
 		with open(source) as population:
-			for i, line in tqdm(enumerate(population), total=get_num_lines(source)):  
-				if i < 10*n and i % n:
+			for i, line in tqdm(enumerate(population), total=num_lines):  
+				if i % n == 0:
 					# print(line)
+					num_samples += 1
 					sample.write(line.strip())
 					sample.write("\n")
+	print("{} samples taken from a population of {} ({:.2f}%)".format(num_samples, num_lines, 100.0*num_samples/num_lines))
 
 
 def main():
-	source = "../../res/head_safebooru.xml"
-	# source = "../../res/safebooru.xml"
+	# source = "../../res/head_safebooru.xml"
+	source = "../../res/safebooru.xml"
 	sample_every_nth_line(source, dest="../../res/sample_safebooru.xml", n=100)
 
 if __name__ == "__main__":
