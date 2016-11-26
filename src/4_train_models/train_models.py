e06adb9dc909d22bef47dbff12a88124e95c7aa4
AlexLamson <alex.lamson@gmail.com>
2016-11-26 01:48:38 -0500

    added decision tree regressor

---
+++ src/4_train_models/train_models.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+import pickle
+import operator
+import time
+import datetime
+from vectorize_data import file_to_xs_ys, load_tag_index_map
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+import pydotplus
+from sklearn import tree
+
+random_state = 1
+
+
+# filename = "../../res/head_safebooru.xml"
+filename = "../../res/sample_safebooru.xml"
+tag_index_map = load_tag_index_map("../../res/tag_index_map.p")
+feature_names = [x[0] for x in sorted(tag_index_map.items(), key=operator.itemgetter(1))]
+xs, ys = file_to_xs_ys(filename, tag_index_map)
+
+
+X_other, X_valid, y_other, y_valid = train_test_split(xs, ys, test_size=10.0/100, random_state=random_state)
+X_train, X_test, y_train, y_test = train_test_split(X_other, y_other, test_size=10.0/90, random_state=random_state)
+
+def save_model(filename, regr):
+	pickle.dump(regr, open(filename, "wb"))
+
+def load_model(filename):
+	regr = pickle.load(open(filename, "rb"))
+	return regr
+
+def timestamp():
+	return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+
+#decision tree regressor
+from sklearn.tree import DecisionTreeRegressor
+regr = DecisionTreeRegressor(max_depth=10, min_samples_split=500, min_samples_leaf=25, random_state=random_state)
+'''
+AREAS OF INTEREST:
+"criterion":["mse","mae"]
+"max_features":["auto","sqrt","log2",1,2]
+"max_depth":[1,3,5,10]
+"min_samples_split":[2,4]
+"min_samples_leaf":[1,4]
+"max_leaf_nodes":[None,10]
+"random_state":[random_state]
+'''
+print("[{}] training decision tree regressor".format(timestamp()))
+regr.fit(X_train, y_train)
+print("[{}] training complete".format(timestamp()))
+save_model("../../res/models/decision_tree.p", regr)
+
+# regr = load_model("../../res/models/decision_tree.p")
+
+# print("inputs:",X_test)
+# print("truth:",y_test)
+# print("predictions:",regr.predict(X_test))
+print("mean_absolute_error:", mean_absolute_error(y_test, regr.predict(X_test)))
+
+print("exporting decision tree regressor visualization")
+dot_data = tree.export_graphviz(regr, feature_names=feature_names, filled=True, out_file=None)
+graph = pydotplus.graph_from_dot_data(dot_data)
+graph.write_pdf("../../figures/regression_tree.pdf")
+
+
+#DEBUG
+#DEBUG
+#DEBUG
+exit()
+#DEBUG
+#DEBUG
+#DEBUG
+
+
+
+#random forest of decision trees regressors
+from sklearn.ensemble import RandomForestRegressor
+regr = RandomForestRegressor(n_estimators=10, max_depth=6, min_samples_split=50, min_samples_leaf=15, random_state=random_state)
+'''
+AREAS OF INTEREST:
+"n_estimators":[10]
+"criterion":["mse","mae"]
+"max_features":["auto","sqrt","log2",1,2]
+"max_depth":[1,3,5,10]
+"min_samples_split":[2,4]
+"min_samples_leaf":[1,4]
+"max_leaf_nodes":[None,10]
+"random_state":[random_state]
+'''
+print("[{}] training random forest regressor".format(timestamp()))
+regr.fit(X_train, y_train)
+print("[{}] training complete".format(timestamp()))
+# print("inputs:",X_test)
+# print("truth:",y_test)
+# print("predictions:",regr.predict(X_test))
+print("mean_absolute_error:", mean_absolute_error(y_test, regr.predict(X_test)))
+save_model("../../res/models/regression_forest.p", regr)
+
+# print("exporting regression forest visualization")
+# dot_data = tree.export_graphviz(regr, feature_names=feature_names, filled=True, out_file=None)
+# graph = pydotplus.graph_from_dot_data(dot_data)
+# graph.write_pdf("../../figures/regression_forest.pdf")
+
+
+
+#PCA(1000->100) features & random forest of decision trees regressors
+
